83052f2a325f82b0d44e9c97dc7088195fd6fe4b
Philip-21 <philipuzomaobiora@gmail.com>
2024-06-13 22:32:45 +0100

    refactoring
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- internal/scheduler/server.go
+++ internal/scheduler/server.go
@@ -1,6 +1,44 @@
 package scheduler
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/Philip-21/dts/internal/proto"
+	"github.com/Philip-21/dts/internal/worker"
+)
+
 //This file will define the gRPC server and implement the methods for scheduling tasks
 
 type Server struct {
-	Task TaskManager
+	pb.UnimplementedSchedulerServer
+	TaskManager *TaskManager
+}
+
+func NewServer(worker *worker.Worker) *Server {
+	return &Server{
+		TaskManager: NewTaskManager(worker),
+	}
+}
+
+func (s *Server) ScheduleOneTimeTask(ctx context.Context, req *pb.ScheduleRequest) (*pb.ScheduleResponse, error) {
+	task := req.GetTask()
+	log.Printf("received task: %+v\n", task)
+	execTask, err := s.TaskManager.OneTimeTask(task.TaskId, task.Payload, task.ExecuteAt)
+	if err != nil {
+		return &pb.ScheduleResponse{
+			Status:  "error",
+			Message: err.Error(),
+		}, err
+	}
+	response := &pb.ScheduleResponse{
+		Status:  "successful",
+		Message: "task scheduled successfully",
+
+		Task: &pb.OneTimeTask{
+			TaskId:  execTask.ID,
+			Payload: execTask.Payload,
+		},
+	}
+	return response, nil
 }
--- internal/scheduler/task_manager.go
+++ internal/scheduler/task_manager.go
@@ -1,17 +1,57 @@
+// manages tasks to be scheduled
 package scheduler
 
-import "sync"
+import (
+	"log"
+	"sync"
+	"time"
 
-//manages tasks to be scheduled
+	"github.com/Philip-21/dts/internal/common"
+	"github.com/Philip-21/dts/internal/worker"
+	"google.golang.org/protobuf/types/known/anypb"
+)
 
 type TaskManager struct {
-	Wg sync.WaitGroup
-	Mt sync.Mutex
+	Wg     sync.WaitGroup
+	Mt     sync.Mutex
+	Tasks  []common.Task
+	Worker *worker.Worker
 }
 
-func NewTaskManager() *TaskManager {
-	t := &TaskManager{}
+func NewTaskManager(worker *worker.Worker) *TaskManager {
+	t := &TaskManager{
+		Worker: worker,
+		Tasks:  make([]common.Task, 0),
+	}
 	return t
 }
 
-func (t*TaskManager) ScheduleOneTimeTask(taskId string){}
+func (t *TaskManager) OneTimeTask(taskId string, payload *anypb.Any, executeAt string) (*common.Task, error) {
+	execTime, err := time.Parse(time.RFC3339, executeAt)
+	if err != nil {
+		return nil, err
+	}
+	task := &common.Task{
+		ID:        taskId,
+		Payload:   payload,
+		ExecuteAt: execTime,
+	}
+	t.Mt.Lock()
+	t.Tasks = append(t.Tasks, *task)
+	t.Mt.Unlock()
+
+	// Schedule the task execution
+	go func() {
+		time.Sleep(time.Until(task.ExecuteAt))
+		executedTask, err := t.Worker.ExecuteOneTimeTask(task.ID)
+		if err != nil {
+			log.Printf("error executing task: %v", err)
+			return
+		}
+		log.Printf("task executed successfully: %+v", executedTask)
+	}()
+
+	return task, nil
+}
+
+func (t *TaskManager) ScheduleRecuringTask() {}
